0e6c0250cb064cc32730c8acb6c9a2e1c6cbe563
Kenec <nnamani.kenechukwu@gmail.com>
2019-03-26 17:19:37 +0100

    try curl inside the app

--- src/main/java/com/example/r42/SuchController.java
+++ src/main/java/com/example/r42/SuchController.java
@@ -13,10 +13,13 @@ public class SuchController {
 	@RequestMapping("/hello")
 	public String suchHello(){
 
+		String azUrl = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
 		String timeUrl = "http://internal-opstest-internal-elb-1231411238.us-east-2.elb.amazonaws.com:3000";
 		RestTemplate restTemplate = new RestTemplate();
+
 		String result = restTemplate.getForObject(timeUrl, String.class);
-		
-		return "Hello " + suchName + " " + result;
+		String azName = restTemplate.getForObject(azUrl, String.class);
+
+		return "Hello " + azName + " " + result;
 	}
 }
