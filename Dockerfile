e4bf6b6f0e56a08d46058b8fbc4073ffa7dc08d8
Kenec <nnamani.kenechukwu@gmail.com>
2019-03-26 16:40:05 +0100

    get the AZ

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM java:8
 EXPOSE 9090
 ADD /target/opstest.jar opstest.jar
-ENTRYPOINT [ "java", "-jar", "opstest.jar" ]
+RUN  --rm -v $(pwd)/mybashscript.sh:/mybashscript.sh ubuntu bash /mybashscript.sh
+ENTRYPOINT [ "java", "-jar", "opstest.jar", "./change.sh" ]
--- change.sh
+++ change.sh
@@ -1,2 +1,4 @@
-#!/bin/bas
-echo "suchname=Kene" > ./src/main/resources/application.properties
+#!/bin/bash
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+echo "suchname=$EC2_AVAIL_ZONE" > ./src/main/resources/application.properties
+echo "server.port=9090" >> ./src/main/resources/application.properties
